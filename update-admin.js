ee03606c8ecd9d62674d062a7e6885a7e892f778
julialvarezmen <julian.alvarezm@sofka.com.co>
2025-12-15 10:47:31 -0500

    Changes Admin

--- update-admin.js
+++
@@ -1,7 +0,0 @@
-db = db.getSiblingDB('restaurant_admin');
-db.users.updateOne(
-  { username: 'admin' }, 
-  { $set: { roles: ['admin', 'waiter', 'chef'] } }
-);
-print('Admin user updated!');
-db.users.findOne({ username: 'admin' }, { password: 0 });
